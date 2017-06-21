556c730dd2c887197214b201816ccdd8585111bd
Bruno E G Barros <begbarros@gmail.com>
2017-06-21 10:12:13 -0300

    132. Create Default Subobject for component - Ai Tanks aiming at player and player can aim everywhere through TankAimingComponent called by the tank

--- BattleTank/Source/BattleTank/Tank.cpp
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Tank.h"
+#include "TankAimingComponent.h"
 
 
 // Sets default values
@@ -8,6 +9,7 @@ ATank::ATank()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	TankAimingComponent = CreateDefaultSubobject <UTankAimingComponent>(FName("Aiming Component"));
 
 }
 
@@ -34,8 +36,7 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-		FString TankName = GetName();
-		UE_LOG(LogTemp, Warning, TEXT("%s aiming at %s"), *TankName, *(HitLocation.ToString()))
-	
+	TankAimingComponent->AimAt(HitLocation);
+
 }
 
